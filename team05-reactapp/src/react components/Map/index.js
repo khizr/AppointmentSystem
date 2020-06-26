f1fd30d6b425170528bf802a51dae97fdf8a2c74
Muhammad Moaz <m.moaz@mail.utoronto,ca>
2020-06-26 13:13:16 -0400

    some intial work on maps page

--- team05-reactapp/src/react components/Map/index.js
+++ team05-reactapp/src/react components/Map/index.js
@@ -1,10 +1,26 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
+import TextField from '@material-ui/core/TextField';
+import SearchIcon from '@material-ui/icons/Search';
+
+import "./styles.css";
 
 /* Component for the Map page */
 class Map extends React.Component {
     render() {
       return (
-        <h1>Hello</h1>
+        <div className="home__bg center">
+          <h1>Search For Your Closest Clinic</h1>
+          <div id= "round-container">
+            <TextField id="filled-basic" label="Enter Your Postal Code" variant="filled"  />
+            <button style={{ borderRadius: 50}} className="button">
+                <SearchIcon />
+            </button>
+          </div>
+          <div id = "round-container2">
+            <h2>Closest Clinics to You Are:</h2>
+          </div>
+        </div>
       );
     }
   }
