dbabc76146ee637025934129495b849035c46e94
Khizr Khan <khizr.khan@mail.utoronto.ca>
2020-07-03 20:05:03 -0400

    added suggestion feature

--- team05-reactapp/src/react components/Chat/index.js
+++ team05-reactapp/src/react components/Chat/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import {uid} from 'react-uid';
 import "./styles.css";
+import HomeIcon from '@material-ui/icons/Home';
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
 
 /* Component for the Chat page */
 class Chat extends React.Component {
@@ -33,19 +36,46 @@ class Chat extends React.Component {
     })
   }
 
+  sendSuggestion = (suggestion) => {
+    this.state.message = suggestion
+    this.sendMessage()
+  }
+
   render() {
     return (
-      <div>
+      <div id="main">
+        <div id="topbar">
+          <Link to={"./"}>
+            <Button variant="contained"
+            color="secondary" 
+            style={{ borderRadius: 50}}
+            className="homeButton"
+            endIcon={<HomeIcon />}>
+            Home</Button>
+          </Link>
+        </div>
         <h1>Chat with us!</h1>
+
+
         <div id="chatbox"> 
+          <h3>What can we help you with?</h3>
+          <div id="suggestions">
+            <span>Popular Requests: </span>
+            <button onClick={() => this.sendSuggestion("May I speak to my family doctor please?")}>request doctor</button>
+            <button onClick={() => this.sendSuggestion("What time is my appointment set for again?")}>appointment reminder</button>
+            <button onClick={() => this.sendSuggestion("Where is the closest clinic to my location?")}>request location</button>
+            <button onClick={() => this.sendSuggestion("Could you please send my receipt to my email?")}>request email receipt</button>
+
+          </div>
 
           <input id="textbox" 
+          placeholder="Type your question or click one of the options above"
           type="text" 
           value = {this.state.message}
           onChange={this.handleInputChange}
           name = "message" 
           />
-          <button onClick={this.sendMessage}>send</button>
+          <button id="sendButton" onClick={this.sendMessage}>send</button>
 
           <ul id='messageList'>
             {this.state.sentMessages.map((msg) => {
@@ -56,6 +86,32 @@ class Chat extends React.Component {
               )
             })}
           </ul>
+        </div>
+
+
+        
+
+        <div id="infoBox">
+          <h3>Contact Information</h3>
+
+          <img src="https://img.icons8.com/ios-glyphs/30/000000/secured-letter.png"/>
+          <h4>EMAIL</h4>
+          <span>cloud@clinics.com</span>
+
+          <img src="https://img.icons8.com/ios-glyphs/30/000000/phone--v1.png"/>
+          <h4>PHONE</h4>
+          <span>012-345-6789</span>
+
+
+          <img src="https://img.icons8.com/ios-glyphs/30/000000/address.png"/>
+          <h4>ADDRESS</h4>
+          <span>333 Oxford Blvd, Toronto, ON, L5M2G7</span>
+
+          <img src="https://img.icons8.com/ios-glyphs/30/000000/clock.png"/>
+          <h4>HOURS</h4>
+          <span>9AM - 4:30PM , Monday to Friday</span>
+          <span>9AM - 1:30PM , Saturday to Sunday</span>
+
         </div>
       </div>
 
